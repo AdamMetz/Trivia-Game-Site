b068468a244c55db80eb75b1e9a439c4d22f20f7
haitaim <76502924+haitaim@users.noreply.github.com>
2021-11-10 21:15:21 -0600

    Implement basic generators

---
+++ src/modules/generator.mjs
@@ -0,0 +1,77 @@
+const questionGenerators = {
+    addition: generateAddition,
+    subtraction: generateSubtraction,
+    multiplication: generateMultiplication,
+    division: generateDivision
+}
+
+function generateQuestions(options) {
+    const numberOfQuestions = 10
+    const questions = new Array(numberOfQuestions);
+
+    for (let i = 0; i < numberOfQuestions; i++) {
+        questions[i] = new Object();
+        const arithmeticOperation = options[randomInteger(0, options.length)];
+        questions[i].arithmeticOperation = arithmeticOperation;
+        ({text: questions[i].text, answer: questions[i].answer} = questionGenerators[arithmeticOperation]());
+    }
+
+    return questions;
+}
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function generateAddition() {
+    const answer = randomInteger(0, 101);
+    const firstOperand = answer - randomInteger(0, answer + 1);
+    const secondOperand = answer - firstOperand;
+
+    return {
+        text: `${firstOperand} + ${secondOperand} = `,
+        answer: answer
+    };
+}
+
+function generateSubtraction() {
+    const firstOperand = randomInteger(0, 101);
+    const secondOperand = randomInteger(0, firstOperand + 1);
+    const answer = firstOperand - secondOperand;
+
+    return {
+        text: `${firstOperand} - ${secondOperand} = `,
+        answer: answer
+    };
+}
+
+function generateMultiplication() {
+    const firstOperand = randomInteger(0, 11);
+    const secondOperand = randomInteger(0, 11);
+    const answer = firstOperand * secondOperand;
+
+    return {
+        text: `${firstOperand} * ${secondOperand} = `,
+        answer: answer
+    };
+}
+
+function generateDivision() {
+    const firstOperand = randomInteger(0, 101);
+    const getSecondOperand = () => {
+        let x = 0;
+        do {
+            x = randomInteger(1, 101);
+        } while (firstOperand % x !== 0);
+        return x;
+    }
+    const secondOperand = getSecondOperand();
+    const answer = firstOperand / secondOperand;
+
+    return {
+        text: `${firstOperand} / ${secondOperand} = `,
+        answer: answer
+    };
+}
+
+export { generateQuestions };
