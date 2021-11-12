d19cb6db92c1b1c96dc3c45fd24252c8e362721e
haitaim <76502924+haitaim@users.noreply.github.com>
2021-11-11 19:03:10 -0600

    Add grade difficulty

--- src/modules/generator.mjs
+++ src/modules/generator.mjs
@@ -6,26 +6,38 @@ const questionGenerators = {
 };
 
 function generateQuestions(options) {
+    const operations = options.operations;
+    const grade = options.grade;
     const numberOfQuestions = 10
     const questions = new Array(numberOfQuestions);
 
     for (let i = 0; i < numberOfQuestions; i++) {
         questions[i] = new Object();
         const question = questions[i];
-        const arithmeticOperation = options[randomInteger(0, options.length)];
+        const arithmeticOperation = operations[randomInteger(0, operations.length)];
         question.arithmeticOperation = arithmeticOperation;
-        ({ text: question.text, answer: question.answer } = questionGenerators[arithmeticOperation]());
+        ({ text: question.text, answer: question.answer } = questionGenerators[arithmeticOperation](grade));
     }
 
-    return questions;
+    return {grade: grade, questions: questions};
 }
 
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function generateAddition() {
-    const answer = randomInteger(0, 101);
+function generateAddition(grade) {
+    const maxRange = () => {
+        if (grade <= 2) {
+            return 11;
+        } else if (grade <= 5) {
+            return 101;
+        } else {
+            return 301;
+        }
+    };
+
+    const answer = randomInteger(0, maxRange());
     const firstOperand = answer - randomInteger(0, answer + 1);
     const secondOperand = answer - firstOperand;
 
@@ -35,8 +47,18 @@ function generateAddition() {
     };
 }
 
-function generateSubtraction() {
-    const firstOperand = randomInteger(0, 101);
+function generateSubtraction(grade) {
+    const maxRange = () => {
+        if (grade <= 2) {
+            return 11;
+        } else if (grade <= 5) {
+            return 101;
+        } else {
+            return 301;
+        }
+    };
+
+    const firstOperand = randomInteger(0, maxRange());
     const secondOperand = randomInteger(0, firstOperand + 1);
     const answer = firstOperand - secondOperand;
 
@@ -46,9 +68,20 @@ function generateSubtraction() {
     };
 }
 
-function generateMultiplication() {
-    const firstOperand = randomInteger(0, 11);
-    const secondOperand = randomInteger(0, 11);
+function generateMultiplication(grade) {
+    const maxRange = () => {
+        if (grade <= 4) {
+            return 11;
+        } else if (grade <= 7) {
+            return 16;
+        } else {
+            return 21;
+        }
+    };
+
+    const maxFactor = maxRange();
+    const firstOperand = randomInteger(0, maxFactor);
+    const secondOperand = randomInteger(0, maxFactor);
     const answer = firstOperand * secondOperand;
 
     return {
@@ -57,8 +90,16 @@ function generateMultiplication() {
     };
 }
 
-function generateDivision() {
-    const firstOperand = randomInteger(0, 101);
+function generateDivision(grade) {
+    const maxRange = () => {
+        if (grade <= 4) {
+            return 11;
+        } else {
+            return 101;
+        }
+    };
+
+    const firstOperand = randomInteger(0, maxRange());
     const getSecondOperand = () => {
         let x = 0;
         do {
