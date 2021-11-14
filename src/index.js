bc859b1f16705c95a59aaa10358c466860e6c262
Adam Metz <alm939@uregina.ca>
2021-11-14 17:45:05 -0600

    Create views for completed game page and profile page

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.listen (port, () => {
 
 app.get("/", (req, res)=>{
     res.render("home.ejs", {logged_in: false})
-})
+});
 
 app.get("/ingame", (req, res)=>{
     res.render("ingame.ejs", {logged_in: true, curr_question_num: 3, question_set_size: 25})
@@ -30,9 +30,9 @@ app.get("/signup", (req, res)=>{
 });
 
 app.get("/profile", (req, res)=>{
-    res.render("profile.ejs", {logged_in: true})
+    res.render("profile.ejs", {logged_in: true, username: "test", grade: 8, date:"November 10, 2021", score:"90% (9/10)", time_taken: "10 minutes 34 seconds", operations_list: "Addition, Subtraction, Multiplication, Division"})
 });
 
 app.get("/completed_game", (req, res)=>{
-    res.render("completed_game.ejs", {logged_in: true})
+    res.render("completed_game.ejs", {logged_in: true, score:"90% (9/10)"})
 });
